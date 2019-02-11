5df7cef9968436e3d2e34f8093728b168e3b182b
Archita Harathi <architaharathi@Architas-MacBook-Pro.local>
2019-02-10 19:54:03 -0500

    Removing the testing of toolbar from the App.js file

--- App.js
+++ App.js
@@ -5,7 +5,6 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <toolbar> </toolbar>
         <Text>Open up App.js to start working on your app!</Text>
       </View>
     );

4b209a98c66c2313779090d8d0b125e8f4a3056f
Archita Harathi <architaharathi@Architas-MacBook-Pro.local>
2019-02-11 08:51:25 -0500

    Adding the icons and finalizing this draft of the shopsearch page

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Image } from 'react-native';
 
 export default class App extends React.Component {
   render() {
@@ -8,6 +8,9 @@ export default class App extends React.Component {
         <Text style={styles.bigShopUHeader}> Let's shopU! </Text>
         <Text style={styles.searchForProducts}> Search for a Product </Text>
         <View style={styles.searchBox}></View>
+        <Image source={require('./images/search_icon.png')} style={styles.searchIconStyle} />
+        <Image source={require('./images/person_icon.png')} style={styles.personIconStyle} />
+        <Image source={require('./images/top_shopping_cart.png')} style={styles.topCartIconStyle} />
       </View>
     );
   }
@@ -25,14 +28,14 @@ const styles = StyleSheet.create({
     color: '#ffffff',
     textAlign: 'center',
     fontFamily: 'Arial',
-    bottom: 160,
+    bottom: 140,
   },
   searchForProducts: {
     color: '#ffffff',
     fontFamily: 'Arial',
-    fontSize: 20,
-    bottom: 38,
-    right: 38,
+    fontSize: 22,
+    bottom: 37,
+    right: 30,
   },
   searchBox: {
     borderRadius: 50,
@@ -45,6 +48,26 @@ const styles = StyleSheet.create({
     height: 55,
     bottom: 75,
   },
+  personIconStyle: {
+    width: 35,
+    height: 35, 
+    bottom: 460,
+    left: 175,
+  },
+  topCartIconStyle: {
+    width: 35,
+    height: 35, 
+    bottom: 487,
+    right: 175,
+  },
+  searchIconStyle: {
+    width: 30,
+    height: 30, 
+    bottom: 120,
+    left: 110,
+  },
+  
+
   
 
 
--- screens/shopsearch.js
+++ screens/shopsearch.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+
 export default class App extends React.Component {
   render() {
     return (
@@ -45,11 +46,14 @@ const styles = StyleSheet.create({
     height: 55,
     bottom: 75,
   },
+
+
+  }
   
 
 
 
 
 
-});
+)
 

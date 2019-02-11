83e678e7480d97c891d5339a4ce0ce98b674c72b
Ijemma Onwuzulike <ijemmaonwuzulike10@gmail.com>
2019-02-11 15:39:05 -0500

    Revert "Shopsearch"

--- App.js
+++ App.js
@@ -1,68 +1,21 @@
 import React from 'react';
-import { StyleSheet, Text, View,Image } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.bigShopUHeader}> Let{ `'` }s shopU! </Text>
-        <Text style={styles.searchForProducts}> Search for a Product </Text>
-        <View style={styles.searchBox}></View>
-        <Image source={require('./images/search_icon.png')} style={styles.searchIconStyle} />
-        <Image source={require('./images/person_icon.png')} style={styles.personIconStyle} />
-        <Image source={require('./images/top_shopping_cart.png')} style={styles.topCartIconStyle} />
+        <Text>Open up App.js to start working on your app!</Text>
       </View>
     );
   }
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ff715B',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  bigShopUHeader: {
-    fontSize: 85,
-    color: '#ffffff',
-    textAlign: 'center',
-    fontFamily: 'Arial',
-    bottom: 140,
-  },
-  searchForProducts: {
-    color: '#ffffff',
-    fontFamily: 'Arial',
-    fontSize: 22,
-    bottom: 37,
-    right: 30,
-  },
-  searchBox: {
-    borderRadius: 50,
-    color: '#ffffff',
-    borderColor: '#ffffff',
-    borderTopRightRadius: 50,
-    borderBottomLeftRadius: 50,
-    borderWidth: 2,
-    width: 300,
-    height: 55,
-    bottom: 75,
-  },
-  personIconStyle: {
-    width: 35,
-    height: 35, 
-    bottom: 460,
-    left: 175,
-  },
-  topCartIconStyle: {
-    width: 35,
-    height: 35, 
-    bottom: 487,
-    right: 175,
-  },
-  searchIconStyle: {
-    width: 30,
-    height: 30, 
-    bottom: 120,
-    left: 110,
   },
 });

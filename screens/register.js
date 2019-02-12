0e0ff71ce6b0f26aa0180f0e6b43ddb09305ac2b
Sanjana Goli <sanjana@Sanjanas-Macbook.local>
2019-02-12 17:08:20 -0500

    updated register screen

--- screens/register.js
+++ screens/register.js
@@ -2,61 +2,116 @@ import React from 'react';
 import { StyleSheet, Text, View, FlatList, Image, ScrollView, AppRegistry, Dimensions} from 'react-native';
 
 export default class App extends React.Component {
-    render(){
-        return(
-            <View>
-                <View style={styles.container}>
-                </View>
-            </View>
-        )
-    }
+  render() {
+    return (
+      <View style={styles.container}>
+        <FlatList
+          vertical={true}
+          data={[{key: 'Username'}, {key: 'Password'}, {key: 'Phone Number'}, {key: 'Email'}]}
+            renderItem={
+              ({item}) => {
+                return(
+                    <View>
+                      <Text style={styles.subHeader}>{item.key}</Text>
+                      <View style={styles.lineStyle}/>
+                    </View>
+                  )
+                }
+              }
 
+         />
+         
+         <View style={styles.purpleBox}/>
+         <Text style={styles.header}>Register Now</Text>
+
+         <View style={styles.submitButton}/>
+         <Text style={styles.submitText}>Submit!</Text>
+
+
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-      position: 'relative',
-      width: Dimensions.get("screen").width,
-      height: 350,
-      backgroundColor: '#FF715B',
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    //alignItems: 'center',
+    //justifyContent: 'center',
+  },
+  purpleBox: {
+    flex: 1,
+    width: Dimensions.get("screen").width,
+    height: 170,
+    backgroundColor: '#605DF1',
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'absolute',
+    top: 0,
+  },
+  header: {
+    position: 'absolute',
+    top: 17,
+    fontSize: 65,
+    fontFamily: 'Arial',
+    alignItems: 'center',
+    textAlign: 'center',
+    justifyContent: 'center',
+    width: Dimensions.get("screen").width,
+    height: 186,
+    color: '#fff',
+  },
+  subHeader:{
+    left: 30,
+    fontFamily: 'Arial',
+    fontSize: 27,
+    marginBottom: 55,
+    top: 190,
+    color: '#FF715B',
+  },
+  lineStyle: {
+    width: 280,
+    height: 3, backgroundColor: '#6DC4E0', 
+    top: 180, 
+    //right: 0,
+    marginBottom: 10,
+    left: 40,
+  },
 
-    },
-    firstHeader: {
-      color: '#fff',
-      marginTop: 90,
-      marginBottom: 30,
-      fontSize: 30,
-    },
-    subHeader: {
-      fontSize: 20,
-      marginTop: 10,
-      marginBottom: 25,
-      color: '#fff'
-      
-    },
-    imageStyle: {
-      width: 130,
-      height: 130,
-      justifyContent: 'space-between',
-      marginRight: 10,
-      marginLeft: 10,
-    },
-    imageText: {
-      fontSize: 20,
+  submitButton: {
+      position:'absolute',
+      width: 240,
+      height: 65,
+      paddingTop:15,
+      left: 40,
+      top: 590,
+      paddingBottom:15,
+      marginLeft:30,
+      marginRight:30,
+      backgroundColor:'#FF715B',
+      borderRadius:50,
+      borderWidth: 1,
+      borderColor: '#FF715B',
       alignItems: 'center',
-      marginRight: 15,
-      marginLeft: 30,
-    },
-  
-    navigation: {
-      flex: 1,
-      zIndex: 10000,
-      justifyContent: 'flex-end',
-      //height: Dimensions.get("screen").height
-      width: Dimensions.get("screen").width,
-      height: 40,
-      position: 'absolute',
-    }
-   
-  });
-  
+      textAlign: 'center',
+      justifyContent: 'center',
+     
+
+  },
+
+  submitText: {
+    position: 'absolute',
+    // width: 156,
+    // height: 37,
+    alignItems: 'center',
+    textAlign: 'center',
+    justifyContent: 'center',
+    top: 605,
+    left: 137,
+    fontSize: 30,
+    fontFamily: 'Arial',
+    color: '#FFFFFF',
+  }
+
+});

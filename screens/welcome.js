6d59b56d1a94508f45b84224fc2815d21bab079d
Ijemma Onwuzulike <ijemmaonwuzulike10@gmail.com>
2019-02-13 10:47:57 -0500

    updated welcome

--- screens/welcome.js
+++ screens/welcome.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 
 
-export default class App extends React.Component {
+export default class Welcome extends React.Component {
   render() {
     return (
       <View style={styles.container}>

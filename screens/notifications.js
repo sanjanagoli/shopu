1751ee552512da0153077c4e4c78595f85dff22c
Archita Harathi <architaharathi@Architas-MacBook-Pro.local>
2019-02-15 20:48:43 -0500

    Making changes to notifications.js screen, but without the simulator so will make final changes later

--- screens/notifications.js
+++ screens/notifications.js
@@ -11,9 +11,7 @@ export default class Shopsearchscreen extends React.Component {
         <View style={styles.slantedBlueRectangle}></View>
         <View style={styles.slantedPurpleRectangle}></View>
         <View style={styles.whiteCard}></View>
-        <View style={styles.searchLine}></View>
-        <Text style={styles.searchText}>     Search for a Product</Text>
-        <Image source={require('./images/search_icon.png')}style={styles.searchIcon} />
+        <Text style={styles.recentOrdersText}>Your recent orders...</Text>
         <Image source={require('./images/top_shopping_cart.png')}style={styles.topShopCart} />
         <Image source={require('./images/profile_head.png')}style={styles.profileHead} />
       </View>
@@ -35,20 +33,65 @@ const styles = StyleSheet.create({
       top: 0, 
       bottom: 0,
       position: 'absolute',
+    },
+    header: {
+      fontSize: 70,
+      color: '#fff',
+      position: 'absolute',
+      top: 80,
+      textAlign: 'center',
+      fontFamily: 'Arial',
+    },
+    slantedBlueRectangle: {
+      position: 'absolute',
+      backgroundColor: '#6DC4E0',
+      transform:([{ rotateZ:'-5deg'}]),
+      width: Dimensions.get("screen").width - 100,
+      height: 80,
+      top: Dimensions.get("screen").height/2 + 100,
+    },
+    slantedPurpleRectangle: {
+      position: 'absolute',
+      backgroundColor: '#6DC4E0',
+      width: Dimensions.get("screen").width - 100,
+      height: 80,
+      top: Dimensions.get("screen").height/2 + 100,
+    },
+    whiteCard: {
+      position: 'absolute',
+      backgroundColor: '#ffffff',
+      width: Dimensions.get("screen").width - 30,
+      height: Dimensions.get("screen").height/2,
+      top: Dimensions.get("screen").height/2 - 120,
+      shadowColor: '#000000',
+      shadowOffset: {
+        width: 0,
+        height: 3,
+      },
+      shadowRadius: 3,
+      shadowOpacity: 0.9,
+    },
+    topShopCart: {
+      position: 'absolute',
+      top: 50,
+      left: 20,
+      height: 30,
+      width: 30,
+    },
+    profileHead: {
+      position: 'absolute',
+      top: 50,
+      right: 20,
+      height: 30,
+      width: 30,
+    },
+    recentOrdersText: {
+      fontSize: 50,
+      position: 'absolute',
+      color: '#fffff',
+      top: Dimensions.get("screen").height/2 + 80,
+      textAlign: 'center',
+      fontFamily: 'Arial',
+
     }
-
-    
-
-
-    
-  
-
-
-
-
-
-
-
-
-
 });

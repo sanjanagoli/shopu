33d1c0bc79d40992bb1c6b9f39d7a9b1e08078b9
Mia G. Seiner <mia.seiner@s-sein011a.local>
2019-02-16 12:11:44 -0500

    primary button

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+
 
 export default class App extends React.Component {
+  _onPressButton() {
+    Alert.alert('You tapped the button!')
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+         <View style={styles.RectangleShapeViewOne}></View>
+         <View style={styles.buttonContainer}>
+          <Button
+            onPress={this._onPressButton}
+            title="Press Me"
+            color = '#fff'
+          
+          />
+            </View>
       </View>
     );
   }
@@ -18,4 +32,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  RectangleShapeViewOne: {
+    marginTop: 5,
+    width: 221,
+    height: 56,
+    top: 600,
+    borderRadius: 2,
+    backgroundColor: '#FF715B',
+    position: 'absolute',
+    },
+    buttonContainer: {
+      margin: 20,
+      top: 225,
+    },
+  
 });
---
+++ components/primary_button.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+
+
+export default class primary_button extends React.Component {
+  _onPressButton() {
+    Alert.alert('You tapped the button!')
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+         <View style={styles.RectangleShapeViewOne}></View>
+         <View style={styles.buttonContainer}>
+          <Button
+            onPress={this._onPressButton}
+            title="Press Me"
+            color = '#fff'
+          
+          />
+            </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  RectangleShapeViewOne: {
+    marginTop: 5,
+    width: 221,
+    height: 56,
+    top: 600,
+    borderRadius: 2,
+    backgroundColor: '#FF715B',
+    position: 'absolute',
+    },
+    buttonContainer: {
+      margin: 20,
+      top: 225,
+    },
+  
+});

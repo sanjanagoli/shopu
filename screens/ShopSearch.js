9a97eb8f7d7755ae83e561a0b617f7e435e6c932
Ijemma Onwuzulike <ijemmaonwuzulike10@gmail.com>
2019-02-19 00:39:31 -0500

    updated ShopSearch

--- screens/ShopSearch.js
+++ screens/ShopSearch.js
@@ -7,12 +7,12 @@ export default class ShopSearch extends React.Component {
     return (
       <View style={styles.container}>
         <View style={styles.orangeRectangle}></View>
-        <Text style={styles.header}>Let's shopU! </Text>
+        <Text style={styles.header}>Let{`'`}s shopU! </Text>
         <View style={styles.slantedBlueRectangle}></View>
         <View style={styles.slantedPurpleRectangle}></View>
         <View style={styles.whiteCard}></View>
         <View style={styles.searchLine}></View>
-        <Text style={styles.searchText}>     Search for a Product</Text>
+        <Text style={styles.searchText}>Search for a Product</Text>
         <Image source={require('./images/search_icon.png')}style={styles.searchIcon} />
         <Image source={require('./images/top_shopping_cart.png')}style={styles.topShopCart} />
         <Image source={require('./images/profile_head.png')}style={styles.profileHead} />

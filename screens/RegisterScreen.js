2ff6feab4c295a4b6453a9c32963f98c76e601eb
Archita Harathi <architaharathi@Architas-MacBook-Pro.local>
2019-02-19 18:01:07 -0500

    Fixing the RegisterScreen page and finalizing it

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -2,20 +2,6 @@ import React from 'react';
 import { StyleSheet, Text, View, Dimensions, TextInput} from 'react-native';
 import PrimaryButton from './../components/primary_button.js';
 
-textLayout = () => {
-  let data=[{key: "Username"}, {key: "Password"}, {key: "Phone Number"}, {key: "Email"}];
-  let dataMap = data.map((item) => {
-    return(
-      <View>
-        <Text style={styles.subHeader}>{item.key}</Text>
-        <TextInput style={styles.textInput}></TextInput>
-        <View style={styles.lineStyle}/>
-      </View>
-    )
-  })
-  return dataMap;
-}
-
 export default class RegisterScreen extends React.Component {
   render() {
     return (
@@ -23,12 +9,29 @@ export default class RegisterScreen extends React.Component {
         <View style={styles.peachBox}>
         <Text style={styles.header}>Register Now!</Text>
         <View style={styles.whiteBox}>
+          <View marginTop={70}>
+            <Text style={styles.subHeader}>Username</Text>
+            <TextInput style={styles.textInput}></TextInput>
+            <View style={styles.lineStyle}/>
+          </View>
           <View>
-            {textLayout()}
+            <Text style={styles.subHeader}>Password</Text>
+            <TextInput style={styles.textInput}></TextInput>
+            <View style={styles.lineStyle}/>
+          </View>
+          <View>
+            <Text style={styles.subHeader}>Phone Number</Text>
+            <TextInput style={styles.textInput}></TextInput>
+            <View style={styles.lineStyle}/>
+          </View>
+          <View>
+            <Text style={styles.subHeader}>Email</Text>
+            <TextInput style={styles.textInput}></TextInput>
+            <View style={styles.lineStyle}/>
           </View>
         <View/>
         <View>
-        <PrimaryButton title={'Register Now!'} backgroundColor={'#6DC4E0'} height={50} fontSize={30}/>
+          <PrimaryButton title={'Submit!'} backgroundColor={'#6DC4E0'} height={60} fontSize={40}/>
         </View>
         </View>
         </View>
@@ -51,7 +54,7 @@ const styles = StyleSheet.create({
   whiteBox: {
     flexDirection: 'column',
     width: Dimensions.get("screen").width*.91,
-    height: Dimensions.get("screen").height*.55,
+    height: Dimensions.get("screen").height*.58,
     shadowColor: '#000000',
     shadowOffset: {
         width: 0,
@@ -76,6 +79,7 @@ const styles = StyleSheet.create({
     fontFamily: 'Arial',
     fontSize: 27,
     marginTop: 20,
+    marginBottom: 10,
     color: '#605DF1',
   },
   textInput: {
@@ -88,29 +92,5 @@ const styles = StyleSheet.create({
     height: 3, 
     backgroundColor: '#605DF1', 
     marginBottom: 20,
-  },
-  primaryButton: {
-    //marginTop:100,
-    backgroundColor:'#6DC4E0',
-    shadowColor: '#000000',
-    height: Dimensions.get('screen').height*0.065,
-    width: Dimensions.get('screen').width*0.5,
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-    shadowOffset: {
-      width: 0,
-      height: 3
-    },
-    shadowRadius: 5,
-     shadowOpacity: 1.0
-  },
-  submitText: {
-    textAlign: 'center',
-    fontSize: 30,
-    fontFamily: 'Arial',
-    color: '#FFFFFF',
-    fontWeight: 'bold',
-    //marginBottom: 20,
   }
 });

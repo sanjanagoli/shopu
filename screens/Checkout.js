e6d370d08d1abe69a05d4a8f1068761367d71d02
Archita Harathi <architaharathi@Architas-MacBook-Pro.local>
2019-02-18 20:35:32 -0500

    Creating the Checkout.js file

---
+++ screens/Checkout.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { StyleSheet, Text, View, Dimensions, Image } from 'react-native';
+
+export default class Checkout extends React.Component {
+  render() {
+    return (
+    <View> </View>
+      )
+    }
+};

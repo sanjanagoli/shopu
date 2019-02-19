ed6d6ab432b557ecaf30b613bf6a17357a6ec71b
Archita Harathi <architaharathi@Architas-MacBook-Pro.local>
2019-02-19 09:39:53 -0500

    Creating a new Register Screen and making changes

---
+++ appstuff.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import FontLoad from './components/FontLoad';
+import Rating from './screens/rating';
+
+export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      fontLoaded: false,
+    }
+  }
+
+  componentWillMount = () => {
+    FontLoad.then((res) => {
+      this.setState({ fontLoaded: true });
+    })
+  }
+
+  render() {
+    return (
+      this.state.fontLoaded ? (
+        <View style={styles.container}>
+          <Text>Open up App.js to start working on your app!</Text>
+        </View>
+    ) : null
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ screens/RegisterScreen.js
@@ -0,0 +1,114 @@
+import React from 'react';
+import { StyleSheet, Text, View, Dimensions, TextInput} from 'react-native';
+
+textLayout = () => {
+  let data=[{key: "Username"}, {key: "Password"}, {key: "Phone Number"}, {key: "Email"}];
+  let dataMap = data.map((item) => {
+    return(
+      <View>
+        <Text style={styles.subHeader}>{item.key}</Text>
+        <TextInput style={styles.textInput}></TextInput>
+        <View style={styles.lineStyle}/>
+      </View>
+    )
+  })
+  return dataMap;
+}
+
+export default class RegisterScreen extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.peachBox}>
+        <Text style={styles.header}>Register Now!</Text>
+        <View style={styles.whiteBox}>
+          <View>
+            {textLayout()}
+          </View>
+        <View/>
+        <View style={styles.submitButton}/>
+        <Text style={styles.submitText}>Submit!</Text>
+        </View>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  peachBox: {
+    width: Dimensions.get("screen").width,
+    height: Dimensions.get("screen").height*.40,
+    backgroundColor: '#FF715B',
+    alignItems: 'center',
+  },
+  whiteBox: {
+    flexDirection: 'column',
+    width: Dimensions.get("screen").width*.91,
+    height: Dimensions.get("screen").height*.55,
+    shadowColor: '#000000',
+    shadowOffset: {
+        width: 0,
+        height: 3
+    },
+    shadowRadius: 5,
+    shadowOpacity: 1.0,
+    backgroundColor: '#FFFFFF',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 50,
+  },
+  header: {
+    color: '#fff',
+    fontFamily: 'Montserrat-SemiBold',
+    fontSize: 60,
+    textAlign: 'center',
+    marginTop: 70,
+    justifyContent: 'center',
+  },
+  subHeader:{
+    fontFamily: 'Arial',
+    fontSize: 27,
+    marginTop: 20,
+    color: '#605DF1',
+  },
+  textInput: {
+    fontFamily: 'Arial',
+    fontSize: 25,
+    color: '#000000',
+  },
+  lineStyle: {
+    width: 280,
+    height: 3, 
+    backgroundColor: '#605DF1', 
+    marginBottom: 20,
+  },
+  submitButton: {
+    //marginTop:100,
+    backgroundColor:'#6DC4E0',
+    shadowColor: '#000000',
+    height: Dimensions.get('screen').height*0.065,
+    width: Dimensions.get('screen').width*0.5,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowOffset: {
+      width: 0,
+      height: 3
+    },
+    shadowRadius: 5,
+     shadowOpacity: 1.0
+  },
+  submitText: {
+    textAlign: 'center',
+    fontSize: 30,
+    fontFamily: 'Arial',
+    color: '#FFFFFF',
+    fontWeight: 'bold',
+    //marginBottom: 20,
+  }
+});

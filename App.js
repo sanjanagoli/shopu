28b060d8dfb38dd648e1371a796a955abfe93258
Archita Harathi <archita.hi@gmail.com>
2019-02-20 22:16:22 -0500

    Cleaning it up

--- App.js
+++ App.js
@@ -6,27 +6,6 @@ import Notifications from './screens/Notifications';
 import ShopSearch from './screens/ShopSearch';
 
 
-
-class HomeScreen extends React.Component {
-  render() {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Home!</Text>
-      </View>
-    );
-  }
-}
-
-class SettingsScreen extends React.Component {
-  render() {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Settings!</Text>
-      </View>
-    );
-  }
-}
-
 class ThirdsScreen extends React.Component {
   render() {
     return (

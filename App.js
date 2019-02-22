e2564535cd85d62f15ce209cd127eb734eebcabd
ijemmao <ijemmao>
2019-02-21 22:17:58 -0500

    reverted APP

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View, FlatList, Dimensions } from 'react-native';
 import FontLoad from './components/FontLoad';
-import ShoppingList from './screens/ShoppingList';
 
 export default class App extends React.Component {
 
@@ -22,8 +21,7 @@ export default class App extends React.Component {
     return (
       this.state.fontLoaded ? (
         <View style={styles.container}>
-          {/* <Text>Open up App.js to start working on your app!</Text> */}
-          <ShoppingList />
+          <Text>Open up App.js to start working on your app!</Text>
         </View>
     ) : null
     )

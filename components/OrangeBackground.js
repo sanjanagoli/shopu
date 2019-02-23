9ae99fe9dbeaacdc950a4235787279c1e882058c
Archita Harathi <archita.hi@gmail.com>
2019-02-23 14:59:55 -0500

    Updating the orange component

--- components/OrangeBackground.js
+++ components/OrangeBackground.js
@@ -7,20 +7,12 @@ export default class OrangeBackground extends React.Component {
 
   render() {
     return (
-         <View style={[styles.container]}>
-            <View style={[styles.peachBox]}></View>
-         </View>
+         <View style={[styles.peachBox]}></View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
   peachBox: {
     width: Dimensions.get("screen").width,
     height: Dimensions.get("screen").height*.40,

dce9cf66aa7035a6cff0d70dd8ab642ac0c95567
Mia G. Seiner <miagseiner@gmail.com>
2019-02-26 11:47:54 -0500

    working on modal

--- App.js
+++ App.js
@@ -1,50 +1,9 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image} from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import FontLoad from './components/FontLoad';
-import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
-import Notifications from './screens/Notifications';
-import ShopSearch from './screens/ShopSearch';
-import DriverSearch from './screens/DriverSearch';
+import Modal from './components/Modal';
 
-
-const TabNavigator = createBottomTabNavigator({
-  ShopSearch: ShopSearch,
-  Notifications: Notifications,
-  DriverSearch: DriverSearch,
-},
-{
-defaultNavigationOptions: ({ navigation }) => ({
-  tabBarIcon: ({ focused }) => {
-    const { routeName } = navigation.state;
-    let imageName;
-    if (routeName === 'ShopSearch') {
-      imageName = require('./assets/images/nav-cart.png');
-      if (focused) {
-        imageName = require('./assets/images/filled_cart.png');
-      }
-    } else if (routeName === 'Notifications') {
-      imageName = require('./assets/images/home_icon.png');
-      if (focused) {
-        imageName = require('./assets/images/filled_house.png');
-      }
-    } else if (routeName === 'DriverSearch') {
-      imageName = require('./assets/images/person_w_bag.png');
-      if (focused) {
-        imageName = require('./assets/images/filled_person.png');
-      }
-    } 
-    return <Image source={imageName} style={{width: 46, height: 42.99, marginTop:25}} />;
-  },
-}),
-tabBarOptions: {
-  showLabel: false,
-},
-}
-);
-
-export default createAppContainer(TabNavigator);
-
-class App extends React.Component {
+export default class App extends React.Component {
 
   constructor(props) {
     super(props);
@@ -63,7 +22,7 @@ class App extends React.Component {
     return (
       this.state.fontLoaded ? (
         <View style={styles.container}>
-          <Text>Open up App.js to start working on your app!</Text>
+          <Modal/>
         </View>
     ) : null
     )
@@ -77,6 +36,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  
 });
-
---
+++ components/Modal.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import { StyleSheet, Text, View, Dimensions, Image } from 'react-native';
+
+
+export default class App extends React.Component {
+
+  render() {
+    return ( 
+        <View style={styles.container}>
+            <View style = {styles.whiteBox}>
+                <Image source ={require('./../assets/images/x.png')} style={styles.x} />
+                <Text style={styles.header}>Select Your Status</Text>
+                <View style = {styles.rectangles}>
+                    <View style = {styles.checkBox}/>
+                    <Text style={styles.rectangleText}>Going to Store</Text>
+                </View>
+                <View style = {styles.rectangles}>
+                    <View style = {styles.checkBox}/>
+                    <Text style={styles.rectangleText}>Checking Out</Text>
+                </View>
+                <View style = {styles.rectangles}>
+                    <View style = {styles.checkBox}/>
+                    <Text style={styles.rectangleText}>En Route</Text>
+                </View>
+                <View style = {styles.rectangles}>
+                    <View style = {styles.checkBox}/>
+                    <Text style={styles.rectangleText}>Delivered</Text>
+                </View>
+            </View>
+        </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'rgba(92, 92, 92, 0.50)',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: Dimensions.get("screen").height,
+    width:Dimensions.get("screen").width,
+  },
+  whiteBox: {
+      backgroundColor: '#fff',
+      width:Dimensions.get("screen").width*.9,
+      height: Dimensions.get("screen").height*.6,
+      justifyContent: 'center',
+      alignItems: 'center',
+  },
+  rectangles: {
+    backgroundColor: '#605DF1',
+    marginTop: 15,
+    width: Dimensions.get("screen").width*.8,
+    height: Dimensions.get("screen").height*.09,
+    shadowOffset: { width: 8, height: 4},
+    shadowColor: '#000000',
+    shadowOpacity: 0.25,
+    shadowRadius: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  rectangleText:{
+    fontSize: 30,
+    color:  '#fff',
+    fontFamily: 'Montserrat-Regular',
+    marginLeft: Dimensions.get("screen").width*.1,
+  },
+  checkBox: {
+    backgroundColor: '#fff',
+    marginTop: 10,
+    width: Dimensions.get("screen").width*.09,
+    height: Dimensions.get("screen").height*.04,
+    borderRadius: 2,
+    // borderColor: '#fff',
+    position: 'absolute',
+    left: Dimensions.get("screen").width*.05,
+  },
+  header: {
+    fontSize: 35,
+    color:  '#6DC4E0',
+    textAlign: 'center',
+    width: Dimensions.get("screen").width,
+    fontFamily: 'Montserrat-Regular',
+    margin: '5% 5%',
+  },
+  x:{
+    width: 25,
+    height: 25,
+    marginRight: Dimensions.get("screen").width*.8,
+    marginTop: Dimensions.get("screen").height*.01,
+  },
+});

46c7c8bf2766f3816564d39c4e36415a1444ea16
Sanjana Goli <sanjana.goli@gmail.com>
2019-02-26 14:24:04 -0500

    toolbar can change depending on what type of page (user or driver)

--- components/Toolbar.js
+++ components/Toolbar.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { StyleSheet, View, Dimensions, Image } from 'react-native';
+import { StyleSheet, View, Dimensions, Image, Text } from 'react-native';
 
 export default class Toolbar extends React.Component {
     render() {
       return (
            <View style={[styles.formatInBox, {marginBottom: this.props.marginBottom}]}>
-              <Image source ={require('./../assets/images/shopping-bag.png')} style={styles.bag}/>
-              <Image source ={require('./../assets/images/personicon.png')} style={styles.person}/>
+            {this.props.pageType ? <Image source ={require('./../assets/images/shopping-bag.png')} style={styles.bag}/> 
+            :<Image source ={require('./../assets/images/scart.png')} style={styles.cart}/>}
+            <Image source ={require('./../assets/images/personicon.png')} style={styles.person}/>
            </View>
       );
     }
@@ -30,6 +31,11 @@ const styles = StyleSheet.create({
     bag:{
         width: 42,
         height: 38,
+        marginLeft: 15,
+    },
+    cart:{
+        width: 44,
+        height: 40,
         marginLeft: 15,
     },
   });

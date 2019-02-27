6f77a0f970c7317eb73861f8b2fd354d88580d60
Mia G. Seiner <miagseiner@gmail.com>
2019-02-27 14:27:02 -0500

    finished with payment

--- App.js
+++ App.js
@@ -1,39 +1,103 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, YellowBox} from 'react-native';
 import FontLoad from './components/FontLoad';
+import { createBottomTabNavigator, createAppContainer, createStackNavigator} from 'react-navigation';
+import Checkout from './screens/Checkout';
+import DriverSearch from './screens/DriverSearch';
+import Item from './screens/Item';
+import Login from './screens/Login';
+import Notifications from './screens/Notifications';
+import Completed from './screens/OrderComplete';
+import Rating from './screens/Rating';
+import Register from './screens/Register';
+import RequestOptions from './screens/RequestOptions';
+import RequestProfile from './screens/RequestProfile';
+import ShoppingList from './screens/ShoppingList';
+import ShopSearch from './screens/ShopSearch';
+import Welcome from './screens/Welcome';
+import YourCart from './screens/YourCart';
 import Payment from './screens/Payment';
 
-export default class App extends React.Component {
+YellowBox.ignoreWarnings([
+  'Require cycle:',
+]);
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      fontLoaded: false,
-    }
-  }
+FontLoad.then((res) => {
+ })
 
-  componentWillMount = () => {
-    FontLoad.then((res) => {
-      this.setState({ fontLoaded: true });
-    })
-  }
-
-  render() {
-    return (
-      this.state.fontLoaded ? (
-        <View style={styles.container}>
-          <Payment/>
-        </View>
-    ) : null
-    )
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const shopStackNavigator = createStackNavigator({
+  ShopSearch: {
+    screen: ShopSearch
+  },
+  Item: {
+    screen: Item
+  },
+  YourCart: {
+    screen: YourCart
+  },
+  Checkout: {
+    screen: Checkout
+  },
+  OrderComplete: {
+    screen: Completed
+  },
+  Rating: {
+    screen: Rating
   },
 });
+
+const driverStackNavigator = createStackNavigator({
+  DriverSearch: {
+    screen: DriverSearch
+  },
+  RequestOptions: {
+    screen: RequestOptions
+  },
+  RequestProfile: {
+    screen: RequestProfile
+  },
+  ShoppingList: {
+    screen: ShoppingList
+  },
+  Rating: {
+    screen: Rating
+  },
+});
+
+const TabNavigator = createBottomTabNavigator({
+  ShopSearch: shopStackNavigator,
+  Notifications: Notifications,
+  DriverSearch: driverStackNavigator,
+},
+{
+defaultNavigationOptions: ({ navigation }) => ({
+  tabBarIcon: ({ focused }) => {
+    const { routeName } = navigation.state;
+    let imageName;
+    if (routeName === 'ShopSearch') {
+      imageName = require('./assets/images/nav-cart.png');
+      if (focused) {
+        imageName = require('./assets/images/filled_cart.png');
+      }
+    } else if (routeName === 'Notifications') {
+      imageName = require('./assets/images/home_icon.png');
+      if (focused) {
+        imageName = require('./assets/images/filled_house.png');
+      }
+    } else if (routeName === 'DriverSearch') {
+      imageName = require('./assets/images/person_w_bag.png');
+      if (focused) {
+        imageName = require('./assets/images/filled_person.png');
+      }
+    } 
+    return <Image source={imageName} style={{width: 46, height: 42.99, marginTop:25}} />;
+  },
+}),
+tabBarOptions: {
+  showLabel: false,
+},
+}
+);
+
+export default createAppContainer(TabNavigator);
+

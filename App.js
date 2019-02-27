4e4b89d37ed93bdb2e424493ac91a2b8ae7a6c17
Mia G. Seiner <miagseiner@gmail.com>
2019-02-27 14:18:30 -0500

    done with modal

--- App.js
+++ App.js
@@ -1,9 +1,50 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image} from 'react-native';
 import FontLoad from './components/FontLoad';
-import Modal from './components/Modal';
+import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
+import Notifications from './screens/Notifications';
+import ShopSearch from './screens/ShopSearch';
+import DriverSearch from './screens/DriverSearch';
 
-export default class App extends React.Component {
+
+const TabNavigator = createBottomTabNavigator({
+  ShopSearch: ShopSearch,
+  Notifications: Notifications,
+  DriverSearch: DriverSearch,
+},
+{
+defaultNavigationOptions: ({ navigation }) => ({
+  tabBarIcon: ({ focused }) => {
+    const { routeName } = navigation.state;
+    let imageName;
+    if (routeName === 'ShopSearch') {
+      imageName = require('./assets/images/nav-cart.png');
+      if (focused) {
+        imageName = require('./assets/images/filled_cart.png');
+      }
+    } else if (routeName === 'Notifications') {
+      imageName = require('./assets/images/home_icon.png');
+      if (focused) {
+        imageName = require('./assets/images/filled_house.png');
+      }
+    } else if (routeName === 'DriverSearch') {
+      imageName = require('./assets/images/person_w_bag.png');
+      if (focused) {
+        imageName = require('./assets/images/filled_person.png');
+      }
+    } 
+    return <Image source={imageName} style={{width: 46, height: 42.99, marginTop:25}} />;
+  },
+}),
+tabBarOptions: {
+  showLabel: false,
+},
+}
+);
+
+export default createAppContainer(TabNavigator);
+
+class App extends React.Component {
 
   constructor(props) {
     super(props);
@@ -22,7 +63,7 @@ export default class App extends React.Component {
     return (
       this.state.fontLoaded ? (
         <View style={styles.container}>
-          <Modal/>
+          <Text>Open up App.js to start working on your app!</Text>
         </View>
     ) : null
     )
@@ -36,4 +77,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  
 });
+
+

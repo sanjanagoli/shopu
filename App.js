7d7d3faec17061d6e1441b599522710333f3fe52
Mia G. Seiner <miagseiner@gmail.com>
2019-02-27 13:57:20 -0500

    done

--- App.js
+++ App.js
@@ -1,47 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
-import { StyleSheet, Text, View } from 'react-native';
-import FontLoad from './components/FontLoad';
-import CTAbutton from './components/CTAbutton';
-
-export default class App extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      fontLoaded: false,
-    }
-  }
-
-  componentWillMount = () => {
-    FontLoad.then((res) => {
-      this.setState({ fontLoaded: true });
-    })
-  }
-
-  render() {
-    return (
-      
-      this.state.fontLoaded ? (
-        <View style={styles.container}>
-	        <CTAbutton backgroundColor={'#605DF1'} title={'Submit!'} height={40}/>
-	      </View>
-    ) : null
-    )
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-
-
-=======
 import { StyleSheet, Text, View, Image, YellowBox} from 'react-native';
 import FontLoad from './components/FontLoad';
 import { createBottomTabNavigator, createAppContainer, createStackNavigator} from 'react-navigation';
@@ -138,4 +95,3 @@ tabBarOptions: {
 );
 
 export default createAppContainer(TabNavigator);
->>>>>>> 2691dd45cb7afa6c3044ebbeccd33af23018d561

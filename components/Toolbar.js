f06851bfe7f9ec3863de09293422c480ab1fde1f
Sonal Butala <sonalbutala18@gmail.com>
2019-04-21 12:23:09 -0400

    finished the toolbar component

--- components/Toolbar.js
+++ components/Toolbar.js
@@ -17,15 +17,15 @@ export default class Toolbar extends React.Component {
     render() {
       return (
            <View style={[styles.formatInBox, {marginBottom: this.props.marginBottom}]}>
-            <View style={{textAlign: 'center'}}>
-                <Text style={styles.titleText}>Hiey girl</Text>
-            </View>
             {this.props.pageType == 'Driver' ?
-            <TouchableOpacity onPress={this.bag}>
-                <Image source ={require('./../assets/images/shopping-bag.png')} style={styles.bag}/> 
-            </TouchableOpacity> 
+            <View>
+                <TouchableOpacity onPress={this.bag}>
+                    <Image source ={require('./../assets/images/shopping-bag.png')} style={styles.bag}/> 
+                </TouchableOpacity>
+            </View> 
             : null}
-            <TouchableOpacity onPress={this.person}>
+            <Text style={styles.titleText}>{this.props.title}</Text>
+            <TouchableOpacity style={{marginLeft: Dimensions.get('screen').width*.8}} onPress={this.person}>
                 <Image source ={require('./../assets/images/personicon.png')} style={styles.person}/> 
             </TouchableOpacity>
            </View>
@@ -36,7 +36,7 @@ export default class Toolbar extends React.Component {
 const styles = StyleSheet.create({
     formatInBox: {
         flexDirection: 'row', 
-        justifyContent: 'space-between', 
+        justifyContent: 'center', 
         width: Dimensions.get('screen').width,
         alignItems: 'center',
         height: Dimensions.get('screen').height*.06,
@@ -47,21 +47,16 @@ const styles = StyleSheet.create({
     person:{
         width: 43,
         height: 40,
-        marginLeft: Dimensions.get('screen').width*.85,
-        marginRight: Dimensions.get('screen').width*.5,
-
     },
     bag:{
         width: 36,
         height: 40,
-        marginLeft: Dimensions.get('screen').width*.04,
-        marginRight: Dimensions.get('screen').width*-.5,
+        marginRight: Dimensions.get('screen').width*-.1,
     },
     titleText:{
         fontFamily: 'Montserrat-SemiBold',
         fontSize: 30,
         color: '#262626',
-        marginLeft: Dimensions.get('screen').width*.45,
-        marginRight: Dimensions.get('screen').width*-45,
+        position: 'absolute',
     }
   });

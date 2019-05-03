ee82148a8b6620d2cb062d9f1aeed3c342f6cd8c
Mia G. Seiner <miagseiner@gmail.com>
2019-05-03 15:38:09 -0400

    removed comments

--- screens/ShopSearch.js
+++ screens/ShopSearch.js
@@ -131,9 +131,7 @@ export default class ShopSearch extends React.Component {
                   <View style={styles.searchLine}/>
                 </View>
                 <View style={styles.button}>
-                  {/* <TouchableOpacity onPress={this.letsgo}> */}
-                    <PrimaryButton onPress={this.letsgo} backgroundColor={'#19C6D1'} title={"Let's Go!"} height={60} fontSize={30}/>
-                  {/* </TouchableOpacity> */}
+                    <PrimaryButton onPress={this.letsgo} backgroundColor={'#19C6D1'} title={"Let's Go!"} height={60} fontSize={30}/>  
                 </View>
           </View> 
           </View>
